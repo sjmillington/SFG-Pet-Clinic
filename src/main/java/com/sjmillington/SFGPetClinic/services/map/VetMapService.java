3d7a2c3d97068f7bf61d7f160a3ed86c5ea75e2d
sblukcic <sblukcic>
2020-07-20 09:26:50 +0100

    added map services. Added banner.

---
+++ src/main/java/com/sjmillington/SFGPetClinic/services/map/VetMapService.java
@@ -0,0 +1,34 @@
+package com.sjmillington.SFGPetClinic.services.map;
+
+import com.sjmillington.SFGPetClinic.model.Vet;
+import com.sjmillington.SFGPetClinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
